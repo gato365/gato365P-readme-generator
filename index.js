b4d66af27d4f174ca8d1a06cf85749fed1289b22
gato365 <immanuelwilliams@gato365.com>
2022-11-19 18:50:38 -0800

    Created outline for inquire

---
+++ index.js
@@ -0,0 +1,9 @@
+// Title
+// Badges
+// Image of
+// Overview (Horizontal Line)
+// Installation (Horizontal Line)
+// Usage (Horizontal Line)
+// Required Knowledge (Horizontal Line)
+// Demonstration (Horizontal Line)
+// Code of Conduct
