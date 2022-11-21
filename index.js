42912b08b8bc3b55ac890a4bdd59bd857ca53948
gato365 <immanuelwilliams@gato365.com>
2022-11-21 06:16:46 -0800

    Added 1 quesiton and outlined the format for the other questions

--- index.js
+++ index.js
@@ -7,21 +7,49 @@ const fs = require('fs');
 
 
 //** Questions to Include**//
-// Title
-// Badges
-// Image of Project
-// Overview (Horizontal Line)
-   // Why was this repo created? - What problem does this repo solve
-   // What learning points should be mentioned?
-   // How is the repo important? - How does the repo stand out?
-// Installation (Horizontal Line)
-// Usage (Horizontal Line)
-// Required Knowledge (Horizontal Line)
-// Demonstration (Horizontal Line)
-// Code of Conduct
+// 1. Title
+// 2. Badges - https://github.com/badges/shields
+// Example ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+// 3. Image of Project
+// 4. Overview (Horizontal Line)
+//~~~~~4a. Why was this repo created? - What problem does this repo solve
+//~~~~~4b. What learning points should be mentioned?
+//~~~~~4c. How is the repo important? - How does the repo stand out?
+// 5. Installation (Horizontal Line)
+// 6. Usage (Horizontal Line)
+// 7. Required Knowledge (Horizontal Line)
+// 8. Demonstration (Horizontal Line)
+// 9. Future Updates
+// 10. Code of Conduct
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // 1.
+    {
+        type: 'input',
+        name: 'repoName',
+        message: 'What is name of your repository?',
+
+    } //,
+    // // 2.
+    // {},
+    // // 3.
+    // {},
+    // // 4.
+    // {},
+    // // 5.
+    // {},
+    // // 6.
+    // {},
+    // // 7.
+    // {},
+    // // 8.
+    // {},
+    // // 9.
+    // {},
+    // // 10.
+    // {}
+];
 
 
 
@@ -29,10 +57,10 @@ const questions = [];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
