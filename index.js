9f7d50be69acce28c3456c714d3f1981df3f1903
gato365 <immanuelwilliams@gato365.com>
2022-11-21 06:03:36 -0800

    Added questions for my template Markdown file

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+
+
+
+//** Questions to Include**//
+// Title
+// Badges
+// Image of Project
+// Overview (Horizontal Line)
+   // Why was this repo created? - What problem does this repo solve
+   // What learning points should be mentioned?
+   // How is the repo important? - How does the repo stand out?
+// Installation (Horizontal Line)
+// Usage (Horizontal Line)
+// Required Knowledge (Horizontal Line)
+// Demonstration (Horizontal Line)
+// Code of Conduct
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +36,7 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
