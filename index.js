c3390049b48fc7f4f9b784536542fcbf448f8011
gato365 <immanuelwilliams@gato365.com>
2022-11-22 08:58:58 -0800

    Added questions to framework

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const fs = require('fs');
 
 
 //** Questions to Include**//
+// 0. Conditional Statements: https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
 // 1. Title
 // 2. Badges - https://github.com/badges/shields
 // Example ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
 // 3. Image of Project
 // 4. Overview (Horizontal Line)
-//~~~~~4a. Why was this repo created? - What problem does this repo solve
+//~~~~~4a. Why was this repo created? - What problem does this repo solve?
 //~~~~~4b. What learning points should be mentioned?
 //~~~~~4c. How is the repo important? - How does the repo stand out?
 // 5. Installation (Horizontal Line)
@@ -23,32 +24,122 @@ const fs = require('fs');
 // 10. Code of Conduct
 
 // TODO: Create an array of questions for user input
+
+// const questions = [{
+//     type: 'list',
+//     name: 'databasetype',
+//     message: 'Choose database :',
+//     choices: ['mongoDB', 'mysql [alpha]', 'firebase [alpha]', 'url [alpha]'],
+//     default: 'mongoDB'
+// },
+// {
+//     type: 'input',
+//     name: 'url',
+//     message: 'Enter the URL',
+//     when: (answers) => answers.databasetype === 'mongoDB'
+// }]
+
 const questions = [
     // 1.
     {
         type: 'input',
         name: 'repoName',
-        message: 'What is name of your repository?',
+        message: 'What is title of your repository?',
 
-    } //,
-    // // 2.
-    // {},
-    // // 3.
-    // {},
-    // // 4.
-    // {},
-    // // 5.
-    // {},
-    // // 6.
-    // {},
-    // // 7.
-    // {},
-    // // 8.
-    // {},
-    // // 9.
-    // {},
-    // // 10.
-    // {}
+    },
+    // 2.
+
+    {
+        type: 'list',
+        name: 'uploadImage',
+        message: '2. Do you have a markup image of desired repo?',
+        choices: ['Yes', 'No']
+    },
+    {
+        type: 'input',
+        name: 'imageName',
+        message: '2a. What is the name of the image?',
+        when: (answers) => answers.uploadImage === 'Yes'
+    },
+    // 3.
+    {
+        type: list,
+        name: 'uploadBadges',
+        message: '3. Are there any badges needed for this repo?',
+        choices: ['Yes', 'No']
+    },
+    // 3a.
+    {
+        type: 'input',
+        name: 'badgeName',
+        message: '3a. What are the names of the badges?',
+        when: (answers) => answers.uploadBadges === 'Yes'
+    },
+
+    // 4a.
+    {
+        type: 'input',
+        name: 'paragraph1',
+        message: 'Why was this repo created? - What problem does this repo solve?',
+    },
+      // 4b.
+    {
+        type: 'input',
+        name: 'paragraph2',
+        message: 'What learning points should be mentioned?',
+    },
+      // 4c.
+    {
+        type: 'input',
+        name: 'paragraph3',
+        message: 'How is the repo important? - How does the repo stand out?',
+    },
+
+    // 5.
+    {
+      type: 'list',
+        name: 'installationNeeded',
+        message: 'Do you need to mention a special installation instruction?',
+    choices: ['Yes', 'No']
+    },
+     // 5a.
+     {
+        type: 'input',
+        name: 'installationInfo',
+        message: '5a. How should the information be installed?',
+        when: (answers) => answers.installationNeeded === 'Yes'
+    },
+    // 6.
+    {
+        type: 'input',
+        name: 'usageDetails',
+        message: 'How should this repository be used? Explain.',
+    },
+    // 7.
+    {
+        type: 'input',
+        name: 'requiredKnowledge',
+        message:'What required knowledge is needed for the usage for this repo?',
+    },
+    // 8.
+    {
+        type: 'input',
+        name: 'demoCode',
+        message:' Upload the code to demonstrate usage of repo.',
+    },
+    // 9.
+    {
+        type: 'list',
+        name: 'updatesOrRefactor',
+        message:'What future updates will be included and/or how will this code be refactored?',
+    },
+    // 10.
+    {
+        type: 'list',
+        name: 'codeConduct',
+        message:'Should you inlude the Code of Conduct',
+    choices: ['Yes', 'No']
+    }
 ];
 
 
@@ -56,15 +147,22 @@ const questions = [
 
 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init() { }
+// // TODO: Create a function to initialize app
+// function init() { }
 
-// Function call to initialize app
-init();
-
-
+// // Function call to initialize app
+// init();
 
 
+
+
+inquirer.prompt(questions).then((answers) => {
+        const filename = `${answers.ordername.toLowerCase().split(' ').join('')}.json`;
+
+        fs.writeFile(filename, JSON.stringify(answers, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
